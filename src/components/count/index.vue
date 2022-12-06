1a9857ffe69ccda0f11cc6574e019cf1adc50c86
wx_orange <wx_orange@orange007.com>
2022-12-06 10:03:34 +0800

    feat: 修改优化代码

--- src/components/count/index.vue
+++ src/components/count/index.vue
@@ -1,11 +1,9 @@
 <template>
   <div class="app-container">
-    <el-card class="box-card">
-      <div>当前数值{{ countComputed }}</div>
-      <div>双倍数值{{ doubleCount }}</div>
-      <el-button type="primary" size="default" @click="countStore.countAdd">+1</el-button>
-      <el-button type="primary" size="default" @click="countStore.countAdd">-1</el-button>
-    </el-card>
+    <div>当前数值{{ countComputed }}</div>
+    <div>双倍数值{{ doubleCount }}</div>
+    <el-button type="primary" size="default" @click="countStore.countAdd">+1</el-button>
+    <el-button type="primary" size="default" @click="countStore.countAdd">-1</el-button>
   </div>
 </template>
 <script setup lang="ts">
@@ -16,10 +14,4 @@ const countStore = useCountStore();
 const countComputed = computed(() => countStore.count);
 const { doubleCount } = storeToRefs(countStore);
 </script>
-<style scoped lang="scss">
-.app-container {
-  .box-card {
-    width: 400px;
-  }
-}
-</style>
+<style scoped lang="scss"></style>
